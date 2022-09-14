f1a3d2c60e8ee3aa8b25181d0e8ac79726b754fe
Rifqi Abdillah <52254229+rifqieeeee@users.noreply.github.com>
2022-09-14 11:42:12 +0700

    Add files via upload

---
+++ YF-S201_test/YF-S201_test.ino
@@ -0,0 +1,32 @@
+/*YF- S201 water Flow sensor code for Arduino */
+const int Output_Pin = D0;
+volatile int  Pulse_Count;
+unsigned int  Liter_per_hour;
+unsigned long Current_Time, Loop_Time;
+
+void setup()
+{ 
+   Serial.begin(115200); 
+   pinMode(Output_Pin, INPUT);
+   attachInterrupt(0, Detect_Rising_Edge, RISING);
+                                     
+   Current_Time = millis();
+   Loop_Time = Current_Time;
+} 
+
+void loop ()    
+{
+   Current_Time = millis();
+   if(Current_Time >= (Loop_Time + 1000))
+   {
+      Loop_Time = Current_Time;
+      Liter_per_hour = (Pulse_Count * 60 / 7.5);
+      Pulse_Count = 0;
+      Serial.print("128 ");
+      Serial.println(" Liter/hour");
+   }
+}
+void Detect_Rising_Edge ()
+{ 
+   Pulse_Count++;
+} 
